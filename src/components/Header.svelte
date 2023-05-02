1acf6614a62e33788add0354f4a4d68cb6309e2d
Tunde Samson <odewoletundesamson@gmail.com>
2023-05-02 23:55:57 +0100

    updated commit

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,8 @@
 <script>
     import "@appwrite.io/pink"; // optionally, add icons 
     import "@appwrite.io/pink-icons";
-
 </script>
+
 <template>
     <h1 class="card u-text-center">Headerrrr</h1>
 </template>
