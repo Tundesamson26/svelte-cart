872dd1160152ea5fce8f4c531fb4b7a76bc5dd21
Tunde Samson <odewoletundesamson@gmail.com>
2023-05-06 00:59:40 +0100

    updated commit

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <main>
-  <d class="u-text-center" style="margin: 50px auto;">
+  <div class="u-text-center" style="margin: 50px auto;">
     <h1 class="u-font-size-32 u-text-center">Svelte Authentication app</h1>
     <div class="u-cross-center">
         <Signup/>
     </div>
-  </d>
+  </div>
 </main>
 
 <style>
