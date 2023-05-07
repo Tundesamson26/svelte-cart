7decadcae15515ebc85faba5915dce0f0034c0a6
Tunde Samson <odewoletundesamson@gmail.com>
2023-05-07 23:10:55 +0100

    signin component added commit

---
+++ src/components/Signin.svelte
@@ -0,0 +1,46 @@
+<section>
+  <div
+    class="card u-cross-center u-width-full-line u-max-width-400"
+    style="margin: 100px auto"
+  >
+    <div class="u-flex u-main-space-between u-cross-center">
+      <h6 class="heading-level-6">Welcome back!</h6>
+      <button class="icon-x" />
+    </div>
+
+    <form method="post" class="form u-margin-block-start-24">
+      <ul class="form-list">
+        <li class="form-item">
+          <!-- svelte-ignore a11y-label-has-associated-control -->
+          <label class="label">Email</label>
+          <div class="input-text-wrapper">
+            <input
+              type="email"
+              class="input-text u-padding-inline-end-56"
+              placeholder="email address"
+              name="email"
+            />
+          </div>
+        </li>
+        <li class="form-item">
+          <!-- svelte-ignore a11y-label-has-associated-control -->
+          <label class="label">Password</label>
+          <div class="input-text-wrapper">
+            <input
+              type="password"
+              class="input"
+              placeholder="password"
+              name="password"
+            />
+          </div>
+        </li>
+      </ul>
+
+      <div class="form-footer">
+        <div class="u-flex u-main-end u-gap-12">
+          <button class="button" type="submit"> Login </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</section>
